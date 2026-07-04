a3641c5740ad65ff4ad073f0705b1f19af977f69
Sahana052003 <sahananreddy52003@gmail.com>
2026-07-04 17:44:40 +0530

    Using useState,props

---
+++ myfirstreact/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+
+let a="ReactJS"
+let b="AngularJS"
+function App() {
+  return (
+    <div>
+      JSX- Javascript XML
+      <h1>MY First React Learning</h1>
+      <h2>I Like  {a},{b}</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ shopping/src/shoppingfolder/data.js
@@ -0,0 +1,23 @@
+export const Gents={
+    title:"Gents Fashion",
+    image1:"assests/Men/download (4).jpg",
+    image2:"assests/Men/download (5).jpg",
+    image3:"assests/Men/download (6).jpg",
+    image4:"assests/Men/download (7).jpg",
+    price1:"559 INR",
+    price2:"459 INR",
+    price3:"659 INR",
+    price4:"959 INR",
+}
+
+export const Ladies={
+    title:"Ladies Fashion",
+    image1:"assests/Women/download.jpg",
+    image2:"assests/Women/download (1).jpg",
+    image3:"assests/Women/download (2).jpg",
+    image4:"assests/Women/download (3).jpg",
+    price1:"359 INR",
+    price2:"759 INR",
+    price3:"427 INR",
+    price4:"562 INR",
+}
